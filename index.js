d73f745d65c100b2d204954b1679cc3cc548c9ef
Ivan Jonas <ivan@jonas.ninja>
2015-11-12 16:22:35 -0500

    implement argument validation in main functions

--- index.js
+++ index.js
@@ -6,21 +6,34 @@ module.exports = {
 }
 
 /**
- * Given an integer n, returns the nth Fibonacci number. The 1st and 2nd Fibonacci numbers are 1, and the 0th Fibonacci
- * number is 0.
+ * Given a non-negative integer n, returns the nth Fibonacci number. The 1st and 2nd Fibonacci numbers are 1, and the
+ * 0th Fibonacci number is 0.
+ * If a non-number, non-integer argument is passed in, returns undefined.
  * @param n integer index of the desired Fibonacci number in the Fibonacci sequence
+ * @return {number|undefined}
  */
 function getNthFib (n) {
   var arr = getFibArray(n)
-  return arr.pop()
+  if (arr !== undefined && arr.length > 0) {
+    return arr.pop()
+  }
+  return undefined
 }
 
 /**
- * Given an integer n, return an array of length n+1 that contains all Fibonacci numbers up to the nth number.
+ * Given a non-negative integer n, return an array of length n+1 that contains all Fibonacci numbers up to the nth
+ * number.
+ * If a non-number, non-integer argument is passed in, returns undefined.
  * @param n the integer index of the largest desired Fibonacci number in the Fibonacci sequence
+ * @return {(Array|undefined)}
  */
 function getFibArray (n) {
   let arr
+
+  if (typeof n !== 'number' || n % 1 !== 0 || n < 0) {
+    // is not a non-negative integer
+    return undefined
+  }
 
   if (n === 0) {
     return [0]
